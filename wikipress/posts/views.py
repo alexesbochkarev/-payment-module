87883277933f7849c195c3c1f367b65b7d42f6a5
Alex Bochkarev <alexes.bochkarev@mail.ru>
2023-02-06 00:33:20 +0400

    Архитектура проекта

---
+++ wikipress/posts/views.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Post, Group
+# Create your views here.
+
+def index(request):
+    template = 'posts/index.html'
+    post_list = Post.objects.select_related('author', 'group').all()
+    return render(request, template, post_list)
